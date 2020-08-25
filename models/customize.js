74a0c450cd38722fddeef16d7a22bc46de18fccf
Aneesha Panigrahi <aneez1100@gmail.com>
2020-08-25 16:29:25 +0530

    /:customizationId Route added

--- models/customize.js
+++ models/customize.js
@@ -1,36 +1,38 @@
 const mongoose = require("mongoose");
 
-const CustomizeSchema = new mongoose.Schema({
-    section: {
+const TagSchema = new mongoose.Schema({
+    tag_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Customization"
+    },
+    name: {//Collar, Cuffs, Pocket
         type: String,
         required: true
     },
-    category: [
-        {
-            name: {
-                type: String,
-                required: true
-            },
-            items: [
-                {
-                    name: {
-                        type: String,
-                        required: true
-                    },
-                    tag: [{
-                        name: {
-                            type: String,
-                            required: true
-                        },
-                        images: []
-                    }]
-                }
-            ]
-        }
-    ]
+    images: []
+}, {
+    timestamps: true
+});
 
+module.exports = mongoose.model("Tag", TagSchema);
+
+const CustomizeSchema = new mongoose.Schema({
+    section: {//Men,Women
+        type: String,
+        required: true
+    },
+    category: {//Casual, Ethnic, Formal
+        type: String,
+        required: true
+    },
+    items: {//Pant, Shirt
+        type: String,
+        required: true
+    },
+    tags: [{type: mongoose.Schema.Types.ObjectId, ref: "Tag"}]
 }, {
     timestamps: true
 });
 
 module.exports = mongoose.model("Customization", CustomizeSchema);
+
--- routes/customization.js
+++ routes/customization.js
@@ -1,96 +1,142 @@
 const router = require("express").Router();
-const Customization = require('../models/customize');
+const Customizations = require('../models/customize');
+const bodyParser = require('body-parser');
+router.use(bodyParser.json());
+const Tag = require('../models/customize')
 
-// router.use(bodyParser.json());
-
-router.route('/')
+router.route('/') //Done
     .get((req, res, next) => {
-        Customization.find({})
-            .then((items) => {
+        Customizations.find({})
+            .populate('tags._id')
+            .then((customizations) => {
                 res.statusCode = 200;
                 res.setHeader('Content-Type', 'application/json');
-                res.json(items);
+                res.json(customizations);
             }, (err) => next(err))
             .catch((err) => next(err));
-    })
+    })//Done
     .post((req, res, next) => {
-        Customization.create(req.body)
-            .then((items) => {
-                console.log('Item Category Created ', items);
+        Customizations.create(req.body)
+            .then((customization) => {
+                console.log('Customization Created ', customization);
                 res.statusCode = 200;
                 res.setHeader('Content-Type', 'application/json');
-                res.json(items);
+                res.json(customization);
             }, (err) => next(err))
             .catch((err) => next(err));
-    });
-
-router.route('/:item_id')
-    .get((req, res, next) => {
-        Customization.findById(req.params.item_id)
-            .then((items) => {
+    })//Done
+    .put((req, res, next) => {
+        res.statusCode = 403;
+        res.end('PUT operation not supported on /customizations');
+    })//Done
+    .delete((req, res, next) => {
+        Customizations.remove({})
+            .then((resp) => {
                 res.statusCode = 200;
                 res.setHeader('Content-Type', 'application/json');
-                res.json(items);
+                res.json(resp);
             }, (err) => next(err))
             .catch((err) => next(err));
-    });
+    });//Done
 
-router.route('/:item_id/customization')
+router.route('/:customizationId')//Done
     .get((req, res, next) => {
-        Customization.findById(req.params.item_id)
-            .then((items) => {
-                if (items != null) {
+        Customizations.findById(req.params.customizationId)
+            .populate('tags._id')
+            .then((customization) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(customization);
+            }, (err) => next(err))
+            .catch((err) => next(err));
+    })//Done
+    .post((req, res, next) => {
+        res.statusCode = 403;
+        res.end('POST operation not supported on /customizations/' + req.params.customizationId);
+    })//Done
+    .put((req, res, next) => {
+        Customizations.findByIdAndUpdate(req.params.customizationId, {
+            $set: req.body
+        }, { new: true })
+            .then((customization) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(customization);
+            }, (err) => next(err))
+            .catch((err) => next(err));
+    })//Done
+    .delete((req, res, next) => {
+        Customizations.findByIdAndRemove(req.params.customizationId)
+            .then((resp) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(resp);
+            }, (err) => next(err))
+            .catch((err) => next(err));
+    });//Done
+router.route('/:customizationId/tags')
+    .get((req, res, next) => {
+        Customizations.findById(req.params.customizationId)
+            .populate('tags')
+            .then((customization) => {
+                if (customization != null) {
                     res.statusCode = 200;
                     res.setHeader('Content-Type', 'application/json');
-                    res.json(items.customizations);
+                    res.json(customization.tags);
                 }
                 else {
-                    err = new Error('Item ' + req.params.item_id + ' not found');
+                    err = new Error('Customization' + req.params.customizationId + ' not found');
                     err.status = 404;
                     return next(err);
                 }
             }, (err) => next(err))
             .catch((err) => next(err));
-    })
-
+    })//Done
     .post((req, res, next) => {
-        Customization.findById(req.params.item_id)
-            .then((items) => {
-                if (items != null) {
-                    req.body.author = req.user._id;
-                    items.comments.push(req.body);
-                    items.save()
-                        .then((items) => {
-                            Customization.findById(items._id)
-                                .then((items) => {
+        Customizations.findById(req.params.customizationId)
+            .then((customization) => {
+                if (customization != null) {
+                    req.body.tag_id = req.params._id;
+                    customization.tags.push(req.body);
+                    customization.save()
+                        .then((customization) => {
+                            Customizations.findById(customization._id)
+                                .populate('tags.tag_id')
+                                .exec()
+                                .then((customization) => {
                                     res.statusCode = 200;
                                     res.setHeader('Content-Type', 'application/json');
-                                    res.json(items);
+                                    res.json(customization);
                                 })
                         }, (err) => next(err));
                 }
                 else {
-                    err = new Error('Item ' + req.params.item_id + ' not found');
+                    err = new Error('Customization ' + req.params.customizationId + ' not found');
                     err.status = 404;
                     return next(err);
                 }
             }, (err) => next(err))
             .catch((err) => next(err));
-    });
+    })
 
-router.route('/:item_id/customization/:customizationId')
-    .get((req, res, next) => {
-        Customization.findById(req.params.item_id)
-            .then((items) => {
-                if (items != null && items.customizations.id(req.params.customizationId) != null) {
-                    res.statusCode = 200;
-                    res.setHeader('Content-Type', 'application/json');
-                    res.json(items.customizations.id(req.params.customizationId));
-                }
-                else if (items == null) {
-                    err = new Error('Item ' + req.params.dishId + ' not found');
-                    err.status = 404;
-                    return next(err);
+    .put((req, res, next) => {
+        res.statusCode = 403;
+        res.end('PUT operation not supported on /dishes/'
+            + req.params.customizationId + '/tags');
+    })
+    .delete((req, res, next) => {
+        Customizations.findById(req.params.customizationId)
+            .then((customization) => {
+                if (customization != null) {
+                    for (var i = (customization.tags.length - 1); i >= 0; i--) {
+                        customization.tags.id(customization.tags[i]._id).remove();
+                    }
+                    customization.save()
+                        .then((customization) => {
+                            res.statusCode = 200;
+                            res.setHeader('Content-Type', 'application/json');
+                            res.json(customization);
+                        }, (err) => next(err));
                 }
                 else {
                     err = new Error('Customization ' + req.params.customizationId + ' not found');
@@ -101,6 +147,101 @@ router.route('/:item_id/customization/:customizationId')
             .catch((err) => next(err));
     });
 
+
+router.route('/:customizationId/tags/:tagId')
+    .get((req, res, next) => {
+        Customizations.findById(req.params.customizationId)
+            .populate('tags._id')
+            .then((customization) => {
+                if (customization != null && customization.tags.id(req.params.tagId) != null) {
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'application/json');
+                    res.json(customization.tags.id(req.params.tagId));
+                }
+                else if (customization == null) {
+                    err = new Error('Customization ' + req.params.customizationId + ' not found');
+                    err.status = 404;
+                    return next(err);
+                }
+                else {
+                    err = new Error('Tag ' + req.params.tagId + ' not found');
+                    err.status = 404;
+                    return next(err);
+                }
+            }, (err) => next(err))
+            .catch((err) => next(err));
+    })
+
+    .post((req, res, next) => {
+        res.statusCode = 403;
+        res.end('POST operation not supported on /customizations/' + req.params.customizationId
+            + '/tags/' + req.params.tagId);
+    })
+
+    .put((req, res, next) => {
+        Customizations.findById(req.params.customizationId)
+            .then((customization) => {
+                if (customization != null && customization.tags.id(req.params.tagId) != null) {
+                    if (req.body.images) {
+                        customization.tags.id(req.params.tagId).images = req.body.images;
+                    }
+                    // if (req.body.name) {
+                    //     customization.tags.id(req.params.tagId).name = req.body.name;
+                    // }
+                    customization.save()
+                        .then((customization) => {
+                            Customizations.findById(customization._id)
+                                .populate('tags._id')
+                                .then((customization) => {
+                                    res.statusCode = 200;
+                                    res.setHeader('Content-Type', 'application/json');
+                                    res.json(customization);
+                                })
+                        }, (err) => next(err));
+                }
+                else if (customization == null) {
+                    err = new Error('Customization ' + req.params.customizationId + ' not found');
+                    err.status = 404;
+                    return next(err);
+                }
+                else {
+                    err = new Error('Comment ' + req.params.tagId + ' not found');
+                    err.status = 404;
+                    return next(err);
+                }
+            }, (err) => next(err))
+            .catch((err) => next(err));
+    })
+    .delete((req, res, next) => {
+        Customizations.findById(req.params.customizationId)
+            .then((customization) => {
+                if (customization != null && customization.tags.id(req.params.tagId) != null) {
+
+                    customization.tags.id(req.params.tagId).remove();
+                    customization.save()
+                        .then((customization) => {
+                            Customizations.findById(customization._id)
+                                .populate('tags._id')
+                                .then((customization) => {
+                                    res.statusCode = 200;
+                                    res.setHeader('Content-Type', 'application/json');
+                                    res.json(customization);
+                                })
+                        }, (err) => next(err));
+                }
+                else if (customization == null) {
+                    err = new Error('Customization ' + req.params.customizationId + ' not found');
+                    err.status = 404;
+                    return next(err);
+                }
+                else {
+                    err = new Error('Tag ' + req.params.tagId + ' not found');
+                    err.status = 404;
+                    return next(err);
+                }
+            }, (err) => next(err))
+            .catch((err) => next(err));
+    });
 
 module.exports = router;
 
